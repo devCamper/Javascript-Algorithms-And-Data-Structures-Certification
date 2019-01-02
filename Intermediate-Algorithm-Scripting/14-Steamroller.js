16a5adfbc03bcdca3186a695f7a87e90077a2d79
devCamper <44700391+devCamper@users.noreply.github.com>
2019-01-02 11:35:32 +0100

    Solve Steamroller

--- Intermediate-Algorithm-Scripting/14-Steamroller.js
+++ Intermediate-Algorithm-Scripting/14-Steamroller.js
@@ -1,6 +1,20 @@
-function steamrollArray(arr) {
+let newArr
+
+function steamrollArray(arr, keep) {
   // I'm a steamroller, baby
-  return arr;
+  if(!keep) newArr = []
+  for(let i = 0; i < arr.length; i++) {
+    if(typeof(arr[i]) == typeof([]) && arr[i].hasOwnProperty('length')) {
+      steamrollArray(arr[i],true);
+    }
+    else {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr;
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+console.log(steamrollArray([[["a"]], [["b"]]]))
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+console.log(steamrollArray([1, [], [3, [[4]]]]))
+console.log(steamrollArray([1, {}, [3, [[4]]]]))
